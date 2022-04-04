ba347f5e556046e2f9b5a00609615b2e8c70a0f4
Bassam Gamal <Bassam.gamal@gmail.com>
2022-04-04 16:36:37 +0200

    Fixing code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,26 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
-    FileOutputStream fos = null;try
-    {
-        fos = new FileOutputStream(new File("may_not_exist.txt"));
-    }catch(
-    FileNotFoundException e)
-    {
-        // ask the user and try again
-    }catch(
-    IOException e)
-    {
-        // more serious, abort
-    }finally
-    {
-        if (fos != null) {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                /* ignore */ }
+    public static void introduceIssue() {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(new File("may_not_exist.txt"));
+        } catch (FileNotFoundException e) {
+            // ask the user and try again
+        } catch (IOException e) {
+            // more serious, abort
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    /* ignore */ }
+            }
         }
     }
 }
