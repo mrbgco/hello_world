89bc2193c067282cff0ae50feb5d0aa8cadb6612
Bassam Gamal <Bassam.gamal@gmail.com>
2022-04-04 16:32:35 +0200

    Introducing an issue

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,4 +2,25 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    FileOutputStream fos = null;try
+    {
+        fos = new FileOutputStream(new File("may_not_exist.txt"));
+    }catch(
+    FileNotFoundException e)
+    {
+        // ask the user and try again
+    }catch(
+    IOException e)
+    {
+        // more serious, abort
+    }finally
+    {
+        if (fos != null) {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                /* ignore */ }
+        }
+    }
 }
